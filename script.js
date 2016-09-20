17b8c5e67cf9b85efe9b33eb7af827b624aa82c1
Ali Azam <aliazam@hotmail.co.uk>
2016-09-20 13:52:56 -0700

    Js Maths Operator 1

---
+++ script.js
@@ -0,0 +1,35 @@
+function plus()
+{
+var a = parseInt(prompt("enter first number"));
+var b = parseInt(prompt("enter second number"));
+var c = a + b;
+alert(c);
+}
+function minus()
+{
+var a = parseInt(prompt("enter first number"));
+var b = parseInt(prompt("enter second number"));
+var c = a - b;
+alert(c);
+}
+function multiply()
+{
+var a = parseInt(prompt("enter first number"));
+var b = parseInt(prompt("enter second number"));
+var c = a * b;
+alert(c);
+}
+function divide()
+{
+var a = parseInt(prompt("enter first number"));
+var b = parseInt(prompt("enter second number"));
+var c = a / b;
+alert(c);
+}
+function modulas()
+{
+var a = parseInt(prompt("enter first number"));
+var b = parseInt(prompt("enter second number"));
+var c = a % b;
+alert(c);
+}
